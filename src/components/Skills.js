83370786e4abe4094c34d7ea1a1d106f9bc92653
Zarif Shahriar <zarif.shahriar@shikho.com>
2026-04-22 08:42:35 +0600

    update

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,66 +1,69 @@
-import React from 'react'
-import { Col, Container, Row } from 'react-bootstrap';
+import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
 import meter1 from "../assets/img/meter1.svg";
 import meter2 from "../assets/img/meter2.svg";
 import meter3 from "../assets/img/meter3.svg";
-import colorSharp from "../assets/img/color-sharp.png"
-
+import colorSharp from "../assets/img/color-sharp.png";
 
 const Skills = () => {
-	const responsive = {
-		superLargeDesktop: {
-			// the naming can be any, depends on you.
-			breakpoint: { max: 4000, min: 3000 },
-			items: 5
-		},
-		desktop: {
-			breakpoint: { max: 3000, min: 1024 },
-			items: 3
-		},
-		tablet: {
-			breakpoint: { max: 1024, min: 464 },
-			items: 2
-		},
-		mobile: {
-			breakpoint: { max: 464, min: 0 },
-			items: 1
-		}
-	};
+  const responsive = {
+    superLargeDesktop: {
+      // the naming can be any, depends on you.
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5,
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+    },
+  };
   return (
     <section className="skill" id="skills">
-        <div className="container">
-            <div className="row">
-                <div className="col-12">
-                    <div className="skill-bx wow zoomIn">
-                        <h2>Skills</h2>
-                        <p>Here is the List of skills I have</p>
-                        <Carousel responsive={responsive} infinite={true} className="owl-carousel owl-theme skill-slider">
-                            <div className="item">
-                                <img src={meter1} alt="Image1" />
-                                <h5>React</h5>
-                            </div>
-                            <div className="item">
-                                <img src={meter2} alt="Image2" />
-                                <h5>Javascript</h5>
-                            </div>
-                            <div className="item">
-                                <img src={meter3} alt="Image3" />
-                                <h5>CSS</h5>
-                            </div>
-                            <div className="item">
-                                <img src={meter3} alt="Image4" />
-                                <h5>React Native</h5>
-                            </div>
-                        </Carousel>
-                    </div>
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <div className="skill-bx wow zoomIn">
+              <h2>Skills</h2>
+              <p>Here is the List of skills I have</p>
+              <Carousel
+                responsive={responsive}
+                infinite={true}
+                className="owl-carousel owl-theme skill-slider"
+              >
+                <div className="item">
+                  <img src={meter1} alt="Image1" />
+                  <h5>React</h5>
                 </div>
+                <div className="item">
+                  <img src={meter2} alt="Image2" />
+                  <h5>Javascript</h5>
+                </div>
+                <div className="item">
+                  <img src={meter3} alt="Image3" />
+                  <h5>CSS</h5>
+                </div>
+                <div className="item">
+                  <img src={meter3} alt="Image4" />
+                  <h5>React Native</h5>
+                </div>
+              </Carousel>
             </div>
+          </div>
         </div>
-        <img className="background-image-left" src={colorSharp} alt="Image" />
+      </div>
+      <img className="background-image-left" src={colorSharp} alt="Image7" />
     </section>
-  )
-}
+  );
+};
 
-export default Skills
+export default Skills;
