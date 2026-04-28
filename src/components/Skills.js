2eb363f5c15e4247d5b88c833f44044c967b5274
Zarif Shahriar <zarif.shahriar@shikho.com>
2026-04-28 21:53:59 +0600

    update--

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Col, Container, Row } from "react-bootstrap";
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
 import meter1 from "../assets/img/meter1.svg";
